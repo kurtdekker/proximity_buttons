f06f0d7f15be3236f1f708ef5c1d0d9bd4997666
Kurtina <kurtdekker@gmail.com>
2017-03-14 06:54:15 -0700

    PathGizmos: some handy scripts

---
+++ proximity_buttons/Assets/PathGizmos/PathOfArray.cs
@@ -0,0 +1,72 @@
+﻿/*
+    The following license supersedes all notices in the source code.
+*/
+
+/*
+    Copyright (c) 2017 Kurt Dekker/PLBM Games All rights reserved.
+
+    http://www.twitter.com/kurtdekker
+    
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are
+    met:
+    
+    Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+    
+    Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+    
+    Neither the name of the Kurt Dekker/PLBM Games nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+    
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+    IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+    TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+    PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+    HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+    TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+    PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class PathOfArray : MonoBehaviour
+{
+	public Transform[] Waypoints;
+
+	void OnDrawGizmos()
+	{
+		if (Waypoints == null)
+		{
+			return;
+		}
+
+		if (Waypoints.Length > 0)
+		{
+			Vector3 offset = Vector3.up * 1.0f;
+			Vector3 last = Vector3.zero;
+			for (int i = 0; i < Waypoints.Length; i++)
+			{
+				Vector3 curr = Waypoints[i].position;
+				Gizmos.color = Color.yellow;
+				Gizmos.DrawLine( curr, curr + offset * 2);
+				if (i > 0)
+				{
+					Gizmos.color = Color.white;
+					Gizmos.DrawLine ( last + offset, curr + offset);
+					Gizmos.color = Color.black;
+					Gizmos.DrawLine ( last, curr);
+				}
+				last = curr;
+			}
+		}
+	}
+}
