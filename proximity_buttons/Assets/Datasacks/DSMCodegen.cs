afb478e47e70db5d15b8d97e64760bde87bd5e2d
Kurtina <kurtdekker@gmail.com>
2019-06-13 07:42:29 -0700

    Datasacks: update to commit 2680c39bf24

--- proximity_buttons/Assets/Datasacks/DSMCodegen.cs
+++ proximity_buttons/Assets/Datasacks/DSMCodegen.cs
@@ -21,8 +21,7 @@ public partial class DSM
 	public static Datasack GameRunning { get { return DSM.I.Get( "GameRunning"); } }
 	public static Datasack HighScore { get { return DSM.I.Get( "HighScore"); } }
 	public static Datasack Score { get { return DSM.I.Get( "Score"); } }
-	public static Datasack UserIntent { get { return DSM.I.Get( "UserIntent"); } }
 	public static Datasack Wave { get { return DSM.I.Get( "Wave"); } }
 }
 
-// Total of 8 datasacks found and processed.
+// Total of 7 datasacks found and processed.
