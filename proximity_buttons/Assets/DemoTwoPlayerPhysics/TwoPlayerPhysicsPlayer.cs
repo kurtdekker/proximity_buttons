2a1172df03cb200d5e3ba53a97de03acd77aab13
Kurtina <kurtdekker@gmail.com>
2020-06-11 07:39:58 -0700

    Move a bunch of stuff around, in preparation for some new demos

---
+++ proximity_buttons/Assets/DemoTwoPlayerPhysics/TwoPlayerPhysicsPlayer.cs
@@ -0,0 +1,160 @@
+﻿/*
+	The following license supersedes all notices in the source code.
+
+	Copyright (c) 2020 Kurt Dekker/PLBM Games All rights reserved.
+
+	http://www.twitter.com/kurtdekker
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are
+	met:
+
+	Redistributions of source code must retain the above copyright notice,
+	this list of conditions and the following disclaimer.
+
+	Redistributions in binary form must reproduce the above copyright
+	notice, this list of conditions and the following disclaimer in the
+	documentation and/or other materials provided with the distribution.
+
+	Neither the name of the Kurt Dekker/PLBM Games nor the names of its
+	contributors may be used to endorse or promote products derived from
+	this software without specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+	IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+	TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+	PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+	HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+	SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+	TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+	PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+	LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoPlayerPhysicsPlayer : MonoBehaviour
+{
+	public float BaseMoveSpeed;
+
+	public int PlayerIndex;
+
+	void Reset()
+	{
+		BaseMoveSpeed = 5.0f;
+	}
+
+	VAButton vabMove;
+
+	void CreateVABs()
+	{
+		if (vabMove) Destroy( vabMove);
+
+		vabMove = gameObject.AddComponent<VAButton>();
+		vabMove.r_downable = MR.SR( 0.00f, 0.20f, 0.50f, 0.80f);
+		vabMove.doClamp = true;
+		vabMove.doNormalize = false;
+		vabMove.label = "MOVE";
+	}
+
+	void Start ()
+	{
+		CreateVABs();
+
+		OrientationChangeSensor.Create( transform, () => { CreateVABs(); });
+	}
+
+	Vector3 LastPlayerMotion = Vector3.zero;
+
+	void UpdateMoving()
+	{
+		if (bMove)
+		{
+			float currentSpeed = BaseMoveSpeed + DSM.Wave.iValue;
+
+			LastPlayerMotion = new Vector3(
+				v3Move.x,
+				0,
+				v3Move.y) * currentSpeed;
+
+			transform.position += LastPlayerMotion * Time.deltaTime;
+
+			float angle = Mathf.Rad2Deg * Mathf.Atan2( LastPlayerMotion.x, LastPlayerMotion.z);
+			transform.rotation = Quaternion.Euler( 0, angle, 0);
+		}
+	}
+
+	Vector3 LastFireDirection = Vector3.forward;
+
+	// fraction of player motion to add to bullets
+	const float PlayerMotionFraction = 0.5f;
+
+	bool bMove;
+	Vector3 v3Move;
+
+	void UpdateGatherTouchInput()
+	{
+		bMove = vabMove.fingerDown;
+		v3Move = vabMove.outputRaw;
+	}
+
+	void UpdateGatherKeyboardInput()
+	{
+		// move
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			bMove = true;
+			v3Move.x = -1;
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			bMove = true;
+			v3Move.x =  1;
+		}
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			bMove = true;
+			v3Move.y = 1;
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			bMove = true;
+			v3Move.y = -1;
+		}
+
+		// move
+		if (Input.GetKey(KeyCode.A))
+		{
+			bMove = true;
+			v3Move.x = -1;
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			bMove = true;
+			v3Move.x =  1;
+		}
+		if (Input.GetKey(KeyCode.W))
+		{
+			bMove = true;
+			v3Move.y = 1;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			bMove = true;
+			v3Move.y = -1;
+		}
+	}
+
+	void Update()
+	{
+		UpdateGatherTouchInput();
+
+		UpdateGatherKeyboardInput();
+
+		UpdateMoving();
+	}
+}
