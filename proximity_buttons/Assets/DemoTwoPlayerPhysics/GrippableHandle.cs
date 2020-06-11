c6c410d6ee3e6963ea9908b5d54925e349e62078
Kurtina <kurtdekker@gmail.com>
2020-06-11 09:31:36 -0700

    start of GrippableHandle

---
+++ proximity_buttons/Assets/DemoTwoPlayerPhysics/GrippableHandle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrippableHandle : MonoBehaviour
+{
+	public float radius = 1.0f;
+
+	void OnDrawGizmos()
+	{
+		Gizmos.DrawWireSphere( transform.position, radius);
+	}
+}
