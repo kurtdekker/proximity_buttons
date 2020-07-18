a6c504e2eba029c42053f0d3a4e7f964ad7ca448
Kurtina <kurtdekker@gmail.com>
2020-07-17 20:04:57 -0700

    SceneUtility

---
+++ proximity_buttons/Assets/ProximityButtons/SceneUtility.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SceneUtility
+{
+	public static IEnumerator LoadAdditionalScenes( string[] scenes)
+	{
+		for (int i = 0; i < scenes.Length; i++)
+		{
+			var sceneName = scenes[i];
+
+			UnityEngine.SceneManagement.SceneManager.LoadScene(
+				sceneName, UnityEngine.SceneManagement.LoadSceneMode.Additive);
+
+			if (i == 0)
+			{
+				yield return null;
+
+				UnityEngine.SceneManagement.SceneManager.SetActiveScene(
+					UnityEngine.SceneManagement.SceneManager.GetSceneByName( sceneName));
+			}
+		}
+	}
+}
