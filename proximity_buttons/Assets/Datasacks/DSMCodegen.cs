0a05b3ad67b3dac47c4f6985ab5b6eda57c95f94
Kurtina <kurtdekker@gmail.com>
2020-12-04 10:31:30 -0800

    rebuilt Datasacks codegen

--- proximity_buttons/Assets/Datasacks/DSMCodegen.cs
+++ proximity_buttons/Assets/Datasacks/DSMCodegen.cs
@@ -19,7 +19,10 @@ public partial class DSM
 	public static Datasack AudioHit { get { return DSM.I.Get( "AudioHit"); } }
 	public static Datasack AudioShoot { get { return DSM.I.Get( "AudioShoot"); } }
 	public static Datasack GameRunning { get { return DSM.I.Get( "GameRunning"); } }
+
+	// Persistent (has .Save field checked in Datasack):
 	public static Datasack HighScore { get { return DSM.I.Get( "HighScore"); } }
+
 	public static Datasack Score { get { return DSM.I.Get( "Score"); } }
 	public static Datasack Wave { get { return DSM.I.Get( "Wave"); } }
 }
