bbafcdd9db9057e9391c22e9e3b409b569de0836
Kurtina <kurtdekker@gmail.com>
2021-03-29 13:34:47 -0700

    flamethrower

---
+++ proximity_buttons/Assets/DemoCameraRotatedControls/FlamerThrower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlamerThrower : MonoBehaviour
+{
+	public ParticleSystem ps;
+
+	void Update ()
+	{
+		bool emit = false;
+
+		emit = Input.GetKey( KeyCode.F);
+
+		if (emit)
+		{
+			int count = (int)Mathf.Round (Random.Range( 100,150) * Time.deltaTime);
+			ps.Emit( count);
+		}
+	}
+}
