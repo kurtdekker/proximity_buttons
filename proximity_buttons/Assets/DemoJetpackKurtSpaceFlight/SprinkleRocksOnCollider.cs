42b093c15b083c327eca99d8b46c0ae6166f6fb9
Kurtina <kurtdekker@gmail.com>
2022-01-04 08:49:52 -0800

    JetpackKurt SpaceFlight demo: let's make a more interesting level!

---
+++ proximity_buttons/Assets/DemoJetpackKurtSpaceFlight/SprinkleRocksOnCollider.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SprinkleRocksOnCollider : MonoBehaviour
+{
+	public GameObjectCollection Rocks;
+
+	public int MinRocks = 200;
+	public int Maxrocks = 400;
+
+	void Start ()
+	{
+		var col = GetComponent<Collider>();
+
+		int numRocks = Random.Range( MinRocks, Maxrocks);
+
+		for (int i = 0; i < numRocks; i++)
+		{
+			var p = col.bounds.center
+				+
+				new Vector3(
+					Random.Range( -col.bounds.extents.x, +col.bounds.extents.x),
+					0,
+					Random.Range( -col.bounds.extents.z, +col.bounds.extents.z))
+					+
+					Vector3.up * 200.0f
+				;
+
+			var ray = new Ray( origin: p, direction: Vector3.down);
+
+			RaycastHit hit;
+			if (col.Raycast( ray, out hit, maxDistance: 1000))
+			{
+				var rock = Instantiate<GameObject>( Rocks.PickNextShuffled(), transform);
+
+				rock.transform.position = hit.point;
+				rock.transform.rotation = Quaternion.Euler (0, Random.Range( 0.0f, 360.0f), 0);
+			}
+		}
+	}
+}
