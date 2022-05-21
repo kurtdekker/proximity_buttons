2ecfa13e0183e3bbfd15b60616a901c3a3efaf32
Kurtina <kurtdekker@gmail.com>
2022-05-20 20:49:18 -0700

    DemoSimpleFlightControls

---
+++ proximity_buttons/Assets/DemoSimpleFlightControls/MakeASmallCity.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// @kurtdekker - a simple city to fly around
+
+public class MakeASmallCity : MonoBehaviour
+{
+	public Material Ground;
+	public Material Building;
+
+	void Start()
+	{
+		var plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+
+		float worldSize = 300.0f;
+
+		// make some plane ground
+		plane.transform.SetParent( transform);
+		plane.transform.localPosition = Vector3.zero;
+		plane.transform.localRotation = Quaternion.identity;
+		plane.transform.localScale = Vector3.one * worldSize / 10;;
+
+		plane.GetComponent<Renderer>().material = Ground;
+
+		// make some building
+		int count = 20;
+		for (int i = 0; i <= count; i++)
+		{
+			float fractionX = (float)i / count;
+			float fractionZ = Random.value;
+
+			float buildingXSize = Random.Range( 5, 10);
+			float buildingZSize = Random.Range( 5, 10);
+
+			float x = Mathf.Lerp( -worldSize, +worldSize, fractionX) / 2;
+			float z = Mathf.Lerp( -worldSize, +worldSize, fractionZ) / 2;
+
+			// height
+			float buildingYSize = Random.Range( 10, 45);
+			float y = buildingYSize / 2;
+
+			var cube = GameObject.CreatePrimitive( PrimitiveType.Cube);
+			cube.transform.SetParent( transform);
+			cube.transform.position = new Vector3( x, y, z);
+			cube.transform.localScale = new Vector3( buildingXSize, buildingYSize, buildingZSize);
+
+			// draw call? what's a draw call? :)
+			var copy = new Material( Building);
+			copy.mainTextureScale = new Vector3( buildingXSize, buildingYSize) / 3;
+
+			cube.GetComponent<Renderer>().material = copy;
+		}
+	}
+}
---
+++ proximity_buttons/Assets/DemoSimpleFlightControls/UltraSimpleFlightControls.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+
+// @kurtdekker
+//
+// Ultra-cheeseball flight control
+//
+// To use:
+//	- stick it on a blank GameObject in your scene
+//	- make sure there is a MainCamera in the scene
+
+public class UltraSimpleFlightControls : MonoBehaviour
+{
+	float speed = 10;
+	float roll = -80;
+	float pitch = 60;
+
+	Rigidbody rb;
+
+	VAButton vab;
+
+	void CreateVABs()
+	{
+		if (vab)
+		{
+			Destroy(vab);
+		}
+
+		vab = gameObject.AddComponent<VAButton>();
+		vab.r_downable = new Rect( 0.0f, 0.2f, 1.0f, 0.8f);
+		vab.doClamp = true;
+		vab.doNormalize = false;
+	}
+
+	void Start()
+	{
+		rb = gameObject.AddComponent<Rigidbody>();
+		rb.useGravity = false;
+
+		var cam = Camera.main;
+		cam.transform.SetParent( transform);
+		cam.transform.localPosition = Vector3.zero;
+		cam.transform.localRotation = Quaternion.identity;
+
+		CreateVABs();
+		OrientationChangeSensor.Create( transform, CreateVABs);
+	}
+
+	// inputs
+	float xm, ym;
+
+	void GatherKeyboardInput()
+	{
+		xm += Input.GetAxis("Horizontal");
+		ym += Input.GetAxis("Vertical");
+	}
+
+	void GatherTouchInput()
+	{
+		if (vab)
+		{
+			if (vab.fingerDown)
+			{
+				var output = vab.output;
+				xm += output.x;
+				ym += output.y;
+			}
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		xm = 0;
+		ym = 0;
+
+		GatherKeyboardInput();
+		GatherTouchInput();
+
+		var rot = rb.rotation;
+
+		rot = rot * Quaternion.Euler( 0, 0, roll * xm * Time.deltaTime);
+		rot = rot * Quaternion.Euler( pitch * ym * Time.deltaTime, 0, 0);
+
+		// copied from / to PSFlightPlayer - keep in sync!!
+		float turn = -rb.transform.right.y * 8;
+		// technically this should be a turn around world coords...
+		rot = rot * Quaternion.Euler( 0, turn * Time.deltaTime, 0);
+
+		rb.MoveRotation( rot);
+
+		float step = speed * Time.deltaTime;
+
+		var pos = rb.position + transform.forward * step;
+
+		rb.MovePosition( pos);
+	}
+}
