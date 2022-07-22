e5c8f07a8d9c0ed476e2d7e756beebe98eb1e898
Kurtina <kurtdekker@gmail.com>
2022-07-22 07:14:55 -0700

    Demo2DMovements of various kinds

---
+++ proximity_buttons/Assets/Demo2DMovements/CarSteer2D.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSteer2D : MonoBehaviour
+{
+	[Header( "Provide or we add.")]
+	public Rigidbody2D rb2d;
+
+	[Header( "Provide or we guess.")]
+	public float RateOfTurning;
+	public float Acceleration;
+	public float Damping = 0.5f;
+	public float MaxSpeed;
+
+	public bool LinkSteeringToSpeed = true;
+
+	float Speed;
+
+	void Start ()
+	{
+		if (!rb2d)
+		{
+			rb2d = GetComponent<Rigidbody2D>();
+			if (!rb2d)
+			{
+				rb2d = gameObject.AddComponent<Rigidbody2D>();
+
+				rb2d.gravityScale = 0.0f;
+			}
+		}
+
+		if (RateOfTurning <= 0)
+		{
+			RateOfTurning = 200;
+		}
+
+		if (Acceleration <= 0)
+		{
+			Acceleration = 10;
+		}
+
+		if (MaxSpeed <= 0)
+		{
+			MaxSpeed = 10;
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		float steer = -Input.GetAxisRaw( "Horizontal");
+		float gas = Input.GetAxisRaw( "Vertical");
+
+		Speed += Acceleration * gas * Time.deltaTime;
+
+		Speed -= Damping * Speed * Time.deltaTime;
+
+		if (Speed > +MaxSpeed)
+		{
+			Speed = +MaxSpeed;
+		}
+		if (Speed < -MaxSpeed)
+		{
+			Speed = -MaxSpeed;
+		}
+
+		if (LinkSteeringToSpeed)
+		{
+			steer *= Speed / MaxSpeed;
+		}
+
+		float angle = rb2d.rotation;
+
+		angle += steer * RateOfTurning * Time.deltaTime;
+
+		rb2d.MoveRotation( angle);
+
+		Vector2 position = rb2d.position;
+
+		position += (Vector2)rb2d.transform.up * Speed * Time.deltaTime;
+
+		rb2d.MovePosition( position);		
+	}
+}
---
+++ proximity_buttons/Assets/Demo2DMovements/SpaceShip2D.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShip2D : MonoBehaviour
+{
+	public float RateOfTurn;
+
+	public float MaxSpeed;
+
+	public float Acceleration;
+	public float Damping;
+
+	void Reset()
+	{
+		RateOfTurn = 180;
+		MaxSpeed = 10;
+		Acceleration = 10;
+		Damping = 0.5f;
+	}
+
+	Rigidbody2D rb2d;
+
+	void Start()
+	{
+		rb2d = gameObject.AddComponent<Rigidbody2D>();
+		// set to zero if you want not gravity
+		rb2d.gravityScale = 0.1f;
+	}
+
+	float inputSteer;
+	bool inputThrust;
+
+	void UpdateGatherInput()
+	{
+		inputSteer = 0;
+		inputThrust = false;
+
+		inputSteer += -Input.GetAxisRaw( "Horizontal");
+		if (Mathf.Abs( Input.GetAxisRaw( "Vertical")) > 0.5f)
+		{
+			inputThrust = true;
+		}
+
+		// TODO: gather other input here
+
+		// gate and normalize steering
+		if (Mathf.Abs( inputSteer) < 0.3)
+		{
+			inputSteer = 0;
+		}
+		if (inputSteer != 0)
+		{
+			inputSteer = Mathf.Sign( inputSteer);
+		}
+	}
+
+	void UpdateSteering()
+	{
+		if (inputSteer != 0)
+		{
+			float angle = rb2d.rotation;
+
+			angle += inputSteer * RateOfTurn * Time.deltaTime;
+
+			rb2d.MoveRotation( angle);
+		}
+	}
+
+	void UpdateThrusting()
+	{
+		Vector2 forward = rb2d.transform.up;
+
+		Vector2 velocity = rb2d.velocity;
+
+		if (inputThrust)
+		{
+			velocity += (forward * Acceleration * Time.deltaTime);
+
+			if (velocity.magnitude > MaxSpeed)
+			{
+				velocity = velocity.normalized * MaxSpeed;
+			}
+		}
+
+		velocity -= velocity * Damping * Time.deltaTime;
+
+		rb2d.velocity = velocity;
+	}
+
+	void FixedUpdate ()
+	{
+		UpdateGatherInput();
+
+		UpdateSteering();
+
+		UpdateThrusting();
+	}
+}
