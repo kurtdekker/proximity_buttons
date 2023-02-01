c6d8fc67d62420b8e3f6c2d250250e86c596a058
Kurtina <kurtdekker@gmail.com>
2023-02-01 14:24:40 -0800

    SeeThroughHole: drag your finger around

---
+++ proximity_buttons/Assets/SeeThroughHole/TouchToPixelPosition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchToPixelPosition : MonoBehaviour
+{
+	public RectTransform CenterInConstantPixelSizeCanvas;
+
+	void Update ()
+	{
+		bool input = false;
+		Vector2 position = Vector2.zero;
+
+		var mts = MicroTouch.GatherMicroTouches();
+
+		// only function with exactly one touch
+		if (mts.Length == 1)
+		{
+			var mt = mts[0];
+
+			position = mt.position;
+
+			input = true;
+		}
+
+		if (input)
+		{
+			CenterInConstantPixelSizeCanvas.position = position;
+		}
+	}
+}
