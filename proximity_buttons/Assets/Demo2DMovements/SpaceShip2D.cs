a5e72c8c3345547e197b139dee5a58dbb4af5608
Kurtina <kurtdekker@gmail.com>
2023-07-12 06:53:06 -0700

    SpaceShip2D updated with audio and particles

--- proximity_buttons/Assets/Demo2DMovements/SpaceShip2D.cs
+++ proximity_buttons/Assets/Demo2DMovements/SpaceShip2D.cs
@@ -11,6 +11,22 @@ public class SpaceShip2D : MonoBehaviour
 	public float Acceleration;
 	public float Damping;
 
+	[Header( "Engine audio loop.")]
+	public AudioSource EngineAudioLoop;
+
+	float originalAudioVolume;
+	float originalAudioPitch;
+
+	[Header( "Thruster particles.")]
+	public ParticleSystem EngineParticleSystem;
+
+	float originalParticleEmissionRate;
+
+	const float engineSnappiness = 12.0f;
+	const float engineNullingTerm = 1.0f;
+	float currentEngineLevel;
+	float desiredEngineLevel;
+
 	void Reset()
 	{
 		RateOfTurn = 180;
@@ -26,6 +42,16 @@ public class SpaceShip2D : MonoBehaviour
 		rb2d = gameObject.AddComponent<Rigidbody2D>();
 		// set to zero if you want not gravity
 		rb2d.gravityScale = 0.1f;
+
+		originalAudioVolume = EngineAudioLoop.volume;
+		originalAudioPitch = EngineAudioLoop.pitch;
+
+		originalParticleEmissionRate = EngineParticleSystem.emission.rateOverTimeMultiplier;
+
+		currentEngineLevel = 0;
+		desiredEngineLevel = 0;
+
+		UpdateEngineAudio();
 	}
 
 	float inputSteer;
@@ -69,12 +95,16 @@ public class SpaceShip2D : MonoBehaviour
 
 	void UpdateThrusting()
 	{
+		desiredEngineLevel = 0;
+
 		Vector2 forward = rb2d.transform.up;
 
 		Vector2 velocity = rb2d.velocity;
 
 		if (inputThrust)
 		{
+			desiredEngineLevel = 1;
+
 			velocity += (forward * Acceleration * Time.deltaTime);
 
 			if (velocity.magnitude > MaxSpeed)
@@ -86,6 +116,35 @@ public class SpaceShip2D : MonoBehaviour
 		velocity -= velocity * Damping * Time.deltaTime;
 
 		rb2d.velocity = velocity;
+
+		// fractional term
+		currentEngineLevel = Mathf.Lerp( currentEngineLevel, desiredEngineLevel, engineSnappiness * Time.deltaTime);
+
+		// additive term
+		currentEngineLevel = Mathf.MoveTowards( currentEngineLevel, desiredEngineLevel, engineNullingTerm * Time.deltaTime);
+	}
+
+	void UpdateEngineAudio()
+	{
+		float pitch = 1.0f + currentEngineLevel / 2.0f;
+
+		float volume = currentEngineLevel;
+
+		// apply the authored terms from the scene
+		pitch *= originalAudioPitch;
+		volume *= originalAudioVolume;
+
+		EngineAudioLoop.pitch = pitch;
+		EngineAudioLoop.volume = volume;
+	}
+
+	void UpdateEngineParticles()
+	{
+		float rate = currentEngineLevel * originalParticleEmissionRate;
+
+		var em = EngineParticleSystem.emission;
+
+		em.rateOverTimeMultiplier = rate;
 	}
 
 	void FixedUpdate ()
@@ -95,5 +154,9 @@ public class SpaceShip2D : MonoBehaviour
 		UpdateSteering();
 
 		UpdateThrusting();
+
+		UpdateEngineAudio();
+
+		UpdateEngineParticles();
 	}
 }
