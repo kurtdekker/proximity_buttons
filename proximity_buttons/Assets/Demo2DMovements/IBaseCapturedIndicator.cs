c7e7809af1e2674ce538654e1cfe06b9e9838c71
Kurtina <kurtdekker@gmail.com>
2023-10-10 19:53:31 -0700

    Clarification of purpose

--- proximity_buttons/Assets/Demo2DMovements/IBaseCapturedIndicator.cs
+++ proximity_buttons/Assets/Demo2DMovements/IBaseCapturedIndicator.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// @kurtdekker - lets things optionally trip
+// some type of indication of base status.
+
 public interface IBaseCapturedIndicator
 {
 	void SetCapturedStatus(bool captured);
