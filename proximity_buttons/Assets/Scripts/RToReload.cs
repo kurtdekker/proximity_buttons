76c68710c08c42608f3d40c241e7d998c4d1101a
Kurtimon <kdekker@kurtimon>
2025-11-08 09:10:27 -0800

    RToReload script

---
+++ proximity_buttons/Assets/Scripts/RToReload.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RToReload : MonoBehaviour
+{
+	void Reset()
+	{
+		name = GetType().ToString();
+	}
+
+	void DoSceneReload()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene(
+			UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			DoSceneReload();
+		}
+	}
+
+	void OnUserIntent( Datasack ds)
+	{
+		switch( ds.Value)
+		{
+		case "ButtonReload":
+			DoSceneReload();
+			break;
+		}
+	}
+
+	void OnEnable()
+	{
+		DSM.UserIntent.OnChanged += OnUserIntent;
+	}
+	void OnDisable()
+	{
+		DSM.UserIntent.OnChanged -= OnUserIntent;
+	}
+}
